ac7ff6c6e30d166873a46786028755bb6c41bee9
LaurenceTest <reylaurencecapoy@gmail.com>
2023-09-15 10:20:21 +0800

    initial v3 name class. unfinished

---
+++ EmployeeProject/src/employee/version3/Name.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employee.version3;
+
+/**
+ *
+ * @author User
+ */
+public class Name {
+    private String first_name;
+    private String middle_name;
+    private String last_name;
+
+    public Name(String first_name, String middle_name, String last_name) {
+        this.first_name = first_name;
+        this.middle_name = middle_name;
+        this.last_name = last_name;
+    }
+    
+    
+}
