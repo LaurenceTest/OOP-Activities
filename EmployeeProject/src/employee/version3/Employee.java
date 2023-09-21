ca9095cebebe56d5cffb6e1bbff427c7d1d3c3ca
LaurenceTest <143805486+LaurenceTest@users.noreply.github.com>
2023-09-21 20:47:41 +0800

    attempt to only push v2 & 3

--- EmployeeProject/src/employee/version3/Employee.java
+++ EmployeeProject/src/employee/version3/Employee.java
@@ -11,39 +11,39 @@ import java.util.Date;
  *
  * @author User
  */
-public class Employee extends Name{
+public class Employee{
     private int empID;
     private Date empDateHired;
     private Date empBirthDate;
+    private Name empName = new Name();
 
     public Employee(int empID, Name empName, Date empDateHired, Date empBirthDate) {
-        super(empName);
         this.empID = empID;
+        this.empName = empName;
         this.empDateHired = empDateHired;
         this.empBirthDate = empBirthDate;
     }
 
     public Employee(int empID, String firstName, String middleName, String lastName, Date empDateHired, Date empBirthDate, String ...suffix) {
-        setFirstName(firstName);
-        setMiddleName(middleName);
-        setLastName(lastName);
-        setSuffix(suffix);
+        empName.setFirstName(firstName);
+        empName.setMiddleName(middleName);
+        empName.setLastName(lastName);
+        empName.setSuffix(suffix);
         this.empID = empID;
         this.empDateHired = empDateHired;
         this.empBirthDate = empBirthDate;
     }
 
     public Employee(int empID, String firstName, String middleName, String lastName, Date empDateHired, Date empBirthDate) {
-        setFirstName(firstName);
-        setMiddleName(middleName);
-        setLastName(lastName);
+        empName.setFirstName(firstName);
+        empName.setMiddleName(middleName);
+        empName.setLastName(lastName);
         this.empID = empID;
         this.empDateHired = empDateHired;
         this.empBirthDate = empBirthDate;
     }
 
     public Employee() {
-        super();
         empID = -1;
         empDateHired = new Date(0,0,0);
         empBirthDate = new Date(0,0,0);
@@ -59,7 +59,7 @@ public class Employee extends Name{
     }
 
     public String getEmpName(){
-        return super.toString();
+        return empName.toString();
     }
 
     public Date getEmpDateHired() {
@@ -80,6 +80,92 @@ public class Employee extends Name{
     
     @Override
     public String toString() {
-        return String.format("Employee ID: %d\nEmployee Name: %s\nEmployee Date Hired: %td/%tm/%ty\nEmployee Birth Date: %td/%tm/%ty\n",empID,getEmpName(),empDateHired,empDateHired,empDateHired,empBirthDate,empBirthDate,empBirthDate);
+        return String.format("Employee ID: %d\nEmployee Name: %s\nEmployee Date Hired: %td/%tm/%ty\nEmployee Birth Date: %td/%tm/%ty\n",empID,getEmpName().toString(),empDateHired,empDateHired,empDateHired,empBirthDate,empBirthDate,empBirthDate);
+    }
+}
+
+class Name {
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String[] suffix = {""};
+
+    public Name(String firstName, String middleName, String lastName, String ...suffix) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.suffix = suffix;
+    }
+
+    public Name(String firstName, String middleName, String lastName) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+    }
+
+    public Name(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Name(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Name(Name self){
+        this.firstName = self.firstName;
+        this.middleName = self.middleName;
+        this.lastName = self.lastName;
+        this.suffix = self.suffix;
+    }
+
+    public Name(){
+        firstName = "";
+        middleName = "";
+        lastName = "";
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSuffix() {
+        String suffixes = "";
+        int i;
+        for(i = 0; i < suffix.length ; i++){
+            suffixes += i + 1 != suffix.length ? suffix[i] + ", ": suffix[i];
+        }
+        return suffixes;
+    }
+
+    public void setSuffix(String[] suffix) {
+        this.suffix = suffix;
+    }
+
+    @Override
+    public String toString() {
+        String middleName = this.middleName != null ? " " + this.middleName.charAt(0) + "." : "";
+        String lastName = this.lastName != null ? " " + this.lastName : "";
+        return firstName + middleName + lastName + (getSuffix().length() > 0 ? " " + getSuffix() : "");
     }
 }
